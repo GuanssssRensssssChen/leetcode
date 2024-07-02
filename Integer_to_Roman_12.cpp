ec1c791b3a7dc64d6705b53a3ba59ff719477d50
CGR <chaunji86@gmail.com>
2024-07-03 01:15:55 +0800

    other solution

--- Integer_to_Roman_12.cpp
+++ Integer_to_Roman_12.cpp
@@ -1,3 +1,4 @@
+// my solution
 class Solution {
 public:
     
@@ -70,5 +71,19 @@ public:
             count--;
         }
         return ret_s;       
+    }
+};
+
+
+//other solution
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
