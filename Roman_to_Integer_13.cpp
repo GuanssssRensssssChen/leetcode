2cb997e06b2b608e7e76c4ff36b0b2d85d55f9c5
CGR <chaunji86@gmail.com>
2024-07-08 01:08:47 +0800

    13 add another sol

--- Roman_to_Integer_13.cpp
+++ Roman_to_Integer_13.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <string>
+#include <map>
+
 using namespace std;
-class Solution {
+
+// Sol#1
+class Solution1 {
 private:
         int ret = 0, cur = 0, flag = 0;
         string S1[9] = {"I","II","III","IV","V","VI","VII","VIII","IX"};
@@ -39,10 +41,67 @@ public:
         return ret;
     }
 };
+// Sol#2 用map
+class Solution2 {
+public:
+    int romanToInt(string s) {
+        map<char, int> m;
+        //unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
+            }
+            else{
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+// Sol#3 最快
+class Solution3 {
+public:
+    int romanToInt(string s) {
+        int map[26]={0},ret=0;
+        for(int i=0;i<s.size();i++){
+            s[i]-='A';
+        }
+        map['M'-'A']=1000;
+        map['D'-'A']=500;
+        map['C'-'A']=100;
+        map['L'-'A']=50;
+        map['X'-'A']=10;
+        map['V'-'A']=5;
+        map['I'-'A']=1;
+        
+        
+        for(int i=0;i<s.size();i++){
+            if(map[s[i]]<map[s[i+1]]){
+                ret-=map[s[i]];
+            }
+            else{
+                ret+=map[s[i]];
+            }
+        }
+        return ret; 
+    }
+};
 
 int main(){
 
-    Solution S1;
+    Solution3 S1;
     cout << S1.romanToInt("MMCCCXCIX");
     return 0;
 }
